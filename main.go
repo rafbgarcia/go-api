45c2bcaf5e99abba51e96da77a3aae34f6974a70
Rafael Garcia <rafbgarcia@gmail.com>
2015-07-13 23:01:19 -0300

    Refactor.

--- main.go
+++ main.go
@@ -2,126 +2,25 @@ package main
 
 import (
     "net/http"
+
     "github.com/gin-gonic/gin"
-    "appengine/datastore"
-    "appengine"
+
+    "github.com/rafbgarcia/go-api/posts"
+    "github.com/rafbgarcia/go-api/handlers"
 )
 
 func init() {
     router := gin.Default()
 
-    router.Use(setOrigin)
+    router.Use(handlers.SetOrigin)
 
-    router.OPTIONS("/posts", optionsHandler)
-    router.OPTIONS("/posts/:id", optionsHandler)
+    router.OPTIONS("/posts", handlers.OptionsHandler)
+    router.OPTIONS("/posts/:id", handlers.OptionsHandler)
 
-    router.GET("/posts", listPosts)
-    router.POST("/posts", createPost)
-    router.PUT("/posts/:id", editPost)
-    router.DELETE("/posts/:id", deletePost)
+    router.GET("/posts", posts.List)
+//    router.POST("/posts", posts.Create)
+//    router.PUT("/posts/:id", posts.Edit)
+//    router.DELETE("/posts/:id", posts.Delete)
 
     http.Handle("/", router)
-}
-
-
-type Post struct {
-    Id string `json:"id" datastore:"-"`
-    Title string `json:"title"`
-    Body string `json:"body"`
-}
-
-func deletePost(c *gin.Context) {
-    gaeContext := appengine.NewContext(c.Request)
-
-    key, _ := datastore.DecodeKey(c.Param("id"))
-
-    if err := datastore.Delete(gaeContext, key); err !=nil {
-        c.JSON(http.StatusBadRequest, gin.H{
-            "error": err.Error(),
-        })
-        return
-    }
-
-    c.AbortWithStatus(http.StatusNoContent)
-}
-
-func createPost(c *gin.Context) {
-    gaeContext := appengine.NewContext(c.Request)
-    post := new(Post)
-
-    c.Bind(&post)
-
-    key := datastore.NewIncompleteKey(gaeContext, "Posts", nil)
-
-    if _, err := datastore.Put(gaeContext, key, post); err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{
-            "error": err.Error(),
-        })
-        return
-    }
-
-    c.JSON(http.StatusCreated, post)
-}
-
-func editPost(c *gin.Context) {
-    gaeContext := appengine.NewContext(c.Request)
-    post := new(Post)
-
-    key, _ := datastore.DecodeKey(c.Param("id"))
-
-    if err := datastore.Get(gaeContext, key, post); err !=nil {
-        c.JSON(http.StatusBadRequest, gin.H{
-            "error": err.Error(),
-        })
-        return
-    }
-
-    c.Bind(&post)
-
-    if _, err := datastore.Put(gaeContext, key, post); err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{
-            "error": err.Error(),
-        })
-        return
-    }
-
-    c.AbortWithStatus(http.StatusNoContent)
-}
-
-func listPosts(c *gin.Context) {
-    gaeContext := appengine.NewContext(c.Request)
-    posts := []*Post{}
-
-    keys, err := datastore.NewQuery("Posts").GetAll(gaeContext, &posts)
-    if err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{
-            "error": err.Error(),
-        })
-        return
-    }
-
-    for i, post := range posts {
-        post.Id = keys[i].Encode()
-    }
-
-    c.JSON(http.StatusOK, posts)
-}
-
-var allowedDomains = []string{
-    "http://localhost:8080",
-    "https://go-web-rafa.appspot.com",
-}
-
-func setOrigin(c *gin.Context) {
-    for _, domain := range allowedDomains {
-        if domain == c.Request.Header.Get("Origin") {
-            c.Header("Access-Control-Allow-Origin", domain)
-            break
-        }
-    }
-}
-
-func optionsHandler(c *gin.Context) {
-    c.Header("Access-Control-Allow-Headers", "Content-Type")
-    c.Header("Access-Control-Allow-Methods", "PUT, DELETE")
 }
---
+++ posts/controller.go
@@ -0,0 +1,94 @@
+package posts
+
+import (
+    "net/http"
+
+    "appengine"
+    "appengine/datastore"
+
+    "github.com/gin-gonic/gin"
+)
+
+
+type Post struct {
+    Id string `json:"id" datastore:"-"`
+    Title string `json:"title"`
+    Body string `json:"body"`
+}
+
+func Delete(c *gin.Context) {
+    gaeContext := appengine.NewContext(c.Request)
+
+    key, _ := datastore.DecodeKey(c.Param("id"))
+
+    if err := datastore.Delete(gaeContext, key); err !=nil {
+        c.JSON(http.StatusBadRequest, gin.H{
+            "error": err.Error(),
+        })
+        return
+    }
+
+    c.AbortWithStatus(http.StatusNoContent)
+}
+
+func Create(c *gin.Context) {
+    gaeContext := appengine.NewContext(c.Request)
+    post := new(Post)
+
+    c.Bind(&post)
+
+    key := datastore.NewIncompleteKey(gaeContext, "Posts", nil)
+
+    if _, err := datastore.Put(gaeContext, key, post); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{
+            "error": err.Error(),
+        })
+        return
+    }
+
+    c.JSON(http.StatusCreated, post)
+}
+
+func Edit(c *gin.Context) {
+    gaeContext := appengine.NewContext(c.Request)
+    post := new(Post)
+
+    key, _ := datastore.DecodeKey(c.Param("id"))
+
+    if err := datastore.Get(gaeContext, key, post); err !=nil {
+        c.JSON(http.StatusBadRequest, gin.H{
+            "error": err.Error(),
+        })
+        return
+    }
+
+    c.Bind(&post)
+
+    if _, err := datastore.Put(gaeContext, key, post); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{
+            "error": err.Error(),
+        })
+        return
+    }
+
+    c.AbortWithStatus(http.StatusNoContent)
+}
+
+func List(c *gin.Context) {
+    gaeContext := appengine.NewContext(c.Request)
+    posts := []*Post{}
+
+    keys, err := datastore.NewQuery("Posts").GetAll(gaeContext, &posts)
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{
+            "error": err.Error(),
+        })
+        return
+    }
+
+    for i, post := range posts {
+        post.Id = keys[i].Encode()
+    }
+
+    c.JSON(http.StatusOK, posts)
+}
